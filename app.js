bcf6f61c21fe7803ff653119b1c6f1ff409a64b6
setthasak11 <120776787+setthasak11@users.noreply.github.com>
2023-02-14 00:37:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const path = require("path");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express()
 
 // #############################################################################
